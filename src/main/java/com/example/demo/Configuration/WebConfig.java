5be961d1681b64be8a199364d06c46edc2e5edf7
Leo <leo@vitex.asia>
2025-05-23 00:03:31 +0700

    fix bug login

--- src/main/java/com/example/demo/Configuration/WebConfig.java
+++ src/main/java/com/example/demo/Configuration/WebConfig.java
@@ -1,50 +1,54 @@
-package com.example.demo.Configuration;
+    package com.example.demo.Configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.config.Customizer;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+    import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+    import org.springframework.security.config.http.SessionCreationPolicy;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.web.cors.CorsConfiguration;
+    import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+    import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.util.Arrays;
-import java.util.List;
+    import java.util.Arrays;
+    import java.util.List;
 
-@Configuration
-@EnableWebMvc
-@EnableWebSecurity
-public class WebConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(request -> {
-                    request
-                            .requestMatchers(
-                                    ("/api/lego-store/user/register"),
-                                    ("/api/lego-store/user/login"),
-                                    ("/api/lego-store/user/loginBasic")
-                            ).permitAll()
-                    .anyRequest().authenticated();
-                }).formLogin(form-> form.disable())
-                .httpBasic(basic-> basic.disable());
-        http.cors(new Customizer<CorsConfigurer<HttpSecurity>>() {
-            @Override
-            public void customize(CorsConfigurer<HttpSecurity> httpSecurityCorsConfigurer) {
-                CorsConfiguration corsConfigurer = new CorsConfiguration();
-                corsConfigurer.setAllowedOrigins(List.of("*"));
-                corsConfigurer.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
-                corsConfigurer.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-                corsConfigurer.setExposedHeaders(List.of("x-auth-token"));
-                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-                source.registerCorsConfiguration("/**", corsConfigurer);
-                httpSecurityCorsConfigurer.configurationSource(source);
-            }
-        });
-        return http.build();
+    @Configuration
+    @EnableWebMvc
+    @EnableWebSecurity
+    public class WebConfig {
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http.csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(request -> {
+                        request
+                                .requestMatchers(
+                                        ("/api/lego-store/user/register"),
+                                        ("/api/lego-store/user/login")
+    //                                    ("/api/lego-store/user/loginBasic")
+                                ).permitAll()
+                        .anyRequest().authenticated();
+                    }).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                    .formLogin(form-> form.disable())
+                    .httpBasic(basic-> basic.disable());
+            http.cors(new Customizer<CorsConfigurer<HttpSecurity>>() {
+                @Override
+                public void customize(CorsConfigurer<HttpSecurity> httpSecurityCorsConfigurer) {
+                    CorsConfiguration corsConfigurer = new CorsConfiguration();
+                    corsConfigurer.setAllowedOrigins(List.of("*"));
+                    corsConfigurer.setAllowedOrigins(List.of("http://localhost:3000"));
+                    corsConfigurer.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
+                    corsConfigurer.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+                    corsConfigurer.setExposedHeaders(List.of("x-auth-token"));
+                    corsConfigurer.setAllowCredentials(true);
+                    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                    source.registerCorsConfiguration("/**", corsConfigurer);
+                    httpSecurityCorsConfigurer.configurationSource(source);
+                }
+            });
+            return http.build();
+        }
     }
-}
