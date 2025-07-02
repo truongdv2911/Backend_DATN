136b4844bede6a591257c1dbb155abfa3dada198
truongdv2911 <dovantruong29112005@gmail.com>
2025-07-02 22:19:54 +0700

    fix logic km, pgg

--- src/main/java/com/example/demo/Repository/Khuyen_mai_Repo.java
+++ src/main/java/com/example/demo/Repository/Khuyen_mai_Repo.java
@@ -2,8 +2,11 @@ package com.example.demo.Repository;
 
 import com.example.demo.Entity.KhuyenMai;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,4 +16,8 @@ public interface Khuyen_mai_Repo extends JpaRepository<KhuyenMai, Integer> {
 
     @Query("SELECT k FROM KhuyenMai k WHERE k.trangThai <> 'isDelete'")
     List<KhuyenMai> findAllKhuyenMaiKhongBiXoa();
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM KhuyenMaiSanPham k WHERE k.khuyenMai.id = :idKM")
+    void deleteByKhuyenMaiId(@Param("idKM") Integer idKM);
 }
